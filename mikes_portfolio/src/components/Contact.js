dae1be805fbf53e81ffff7164205483fa04d78d0
Mike VanSleen <mike.vansleen@gmail.com>
2019-10-20 17:28:11 -0600

    Contact section created

--- mikes_portfolio/src/components/Contact.js
+++ mikes_portfolio/src/components/Contact.js
@@ -1,7 +1,24 @@
 import React from "react"
+import phone from "../assets/icons/phone.svg"
+import contactPhone from "../assets/icons/contact-phone.svg"
+import message from "../assets/icons/message.svg"
 
 const Contact = () => {
-  return <div className="contact-container">Contact!</div>
+  return (
+    <div className="contact-container">
+      <img src={phone} />
+      <h2>Contact</h2>
+      <h3>Reach me by phone, email or carrier pigeon</h3>
+      <div className="contact-info-container">
+        <img className="phone" src={contactPhone} />
+        <p>(786) 286-8944</p>
+      </div>
+      <div className="contact-info-container">
+        <img className="message" src={message} />
+        <p>mike.vansleen@gmail.com</p>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
