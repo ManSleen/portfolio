d07fba27e288667c4a0da8995a0e12310dc1629c
Mike VanSleen <mike.vansleen@gmail.com>
2019-10-27 13:29:17 -0600

    Added multiple transitions to elements

--- mikes_portfolio/src/pages/how-to.js
+++ mikes_portfolio/src/pages/how-to.js
@@ -1,12 +1,18 @@
 import React from "react"
 import Header from "../components/header"
-import Project from "../components/project"
+import Project from "../components/Project.js"
 
 const HowTo = () => {
   return (
     <>
       <Header />
-      <Project />
+      <iframe
+        style={{ border: "none" }}
+        width="800"
+        height="450"
+        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FH2RlU96r30K3HM0cC4ETMw%2FHow-To-UI-Design%3Fnode-id%3D1%253A3"
+        allowfullscreen
+      ></iframe>
     </>
   )
 }
